7b251a735d661577a95f1f57bc959fd33be7a7ba
vdulevich <vdulevich@users.noreply.github.com>
2016-01-17 15:59:20 -0500

    Update loadUser.js

--- middleware/loadUser.js
+++ middleware/loadUser.js
@@ -1,11 +1,11 @@
-/*var User = require('../models/User');*/
+var User = require('../models/User');
 
 module.exports = function(req, res, next){
     req.user = res.locals.user = null;
     if(!req.session.user) return next();
-    /*User.findById(req.session.user, function(err, user){
+    User.findById(req.session.user, function(err, user){
         if(err) next(err);
         req.user = res.locals.user = user;
         next();
-    });*/
+    });
 }
